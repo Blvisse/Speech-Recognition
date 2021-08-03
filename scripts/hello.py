7242b9a27902a4b3d2f9753c1707464831b61e49
Blvisse <blaisepke@gmail.com>
2021-08-03 14:29:38 +0300

    running google tests

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,1 +1,11 @@
-print ("Hello world.py")
+import dvc.api
+data=dvc.api.get_url('data/train','https://github.com/Blvisse/Speech-Recognition',rev='train-v0')
+import os
+
+arr = os.listdir(data)
+print(arr)
+
+
+##comment section
+
+print (data)
